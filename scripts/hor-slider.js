cc7db1e4ad6e5cc053522369a8029214565bc23b
DmiKhn <102995269+DmiKhn@users.noreply.github.com>
2022-10-15 01:10:48 +0300

    Add files via upload

--- scripts/hor-slider.js
+++ scripts/hor-slider.js
@@ -1,17 +1,16 @@
-  const menuTitle = $('.hor-slider__maintitle');
+  const sliderSection = $('.hor-slider__wrapper');
 
   $('.hor-slider__link').click((e) => {
     e.preventDefault();
 
     const curItem = $(e.target).closest('.hor-slider__item');
     
-
+    sliderSection.toggleClass('hor-slider__wrapper--active');
     curItem.toggleClass('hor-slider__item--active').siblings().removeClass('hor-slider__item--active');
-    menuTitle.css('display', 'none');
   });
 
   $('.hor-slider__close').click((e) => {
     e.preventDefault();
+    sliderSection.toggleClass('hor-slider__wrapper--active');
     $(e.target).closest('.hor-slider__item').removeClass('hor-slider__item--active');
-    menuTitle.css('display', 'block');
   });
---
+++ scripts/player.js
@@ -0,0 +1,56 @@
+let iframe = document.querySelector('iframe');
+let player = new Vimeo.Player(iframe);
+const playerContainer = $('.player');
+
+let formatTime = (timeSec) => {
+  const roundTime = Math.round(timeSec);
+
+  const minutes = addZero(Math.floor(roundTime / 60));
+  const seconds = addZero(roundTime - minutes * 60);
+
+  function addZero(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  return `${minutes}`+':'+`${seconds}`;
+}
+
+
+player.getCurrentTime().then((seconds) => { 
+  let interval;
+
+  if (typeof interval !== 'undefined') {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    let completedSec = seconds;
+    $('.player__duration-completed').text(formatTime(completedSec));
+    // console.log(completedSec);
+  }, 1000);
+});
+
+
+
+const durationSec = player.getDuration().then((dur) => {
+  
+  $('.player__duration-estimate').text(formatTime(dur));
+});
+
+let eventsInit = () => {
+
+  $('.player__start').click(e => {
+    e.preventDefault();
+
+    if (playerContainer.hasClass('paused')) {
+      playerContainer.removeClass('paused');
+      player.pause();
+    } else {
+      playerContainer.addClass('paused');
+      player.play();
+    }
+    
+  });
+};
+
+eventsInit();
