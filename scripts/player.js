2b2ef4d8a122694a29d37be5dcee79512f789adc
DmiKhn <102995269+DmiKhn@users.noreply.github.com>
2022-10-15 19:20:17 +0300

    Add files via upload

--- scripts/player.js
+++ scripts/player.js
@@ -1,3 +1,60 @@
+// let iframe = document.querySelector('iframe');
+// let player = new Vimeo.Player(iframe);
+// const playerContainer = $('.player');
+
+// let formatTime = (timeSec) => {
+//   const roundTime = Math.round(timeSec);
+
+//   const minutes = addZero(Math.floor(roundTime / 60));
+//   const seconds = addZero(roundTime - minutes * 60);
+
+//   function addZero(num) {
+//     return num < 10 ? `0${num}` : num;
+//   }
+
+//   return `${minutes}`+':'+`${seconds}`;
+// }
+
+
+// player.getCurrentTime().then((seconds) => { 
+//   let interval;
+
+//   if (typeof interval !== 'undefined') {
+//     clearInterval(interval);
+//   }
+
+//   interval = setInterval(() => {
+//     let completedSec = seconds;
+//     $('.player__duration-completed').text(formatTime(completedSec));
+//     // console.log(completedSec);
+//   }, 1000);
+// });
+
+
+
+// const durationSec = player.getDuration().then((dur) => {
+  
+//   $('.player__duration-estimate').text(formatTime(dur));
+// });
+
+// let eventsInit = () => {
+
+//   $('.player__start').click(e => {
+//     e.preventDefault();
+
+//     if (playerContainer.hasClass('paused')) {
+//       playerContainer.removeClass('paused');
+//       player.pause();
+//     } else {
+//       playerContainer.addClass('paused');
+//       player.play();
+//     }
+    
+//   });
+// };
+
+// eventsInit();
+
 let iframe = document.querySelector('iframe');
 let player = new Vimeo.Player(iframe);
 const playerContainer = $('.player');
@@ -15,8 +72,24 @@ let formatTime = (timeSec) => {
   return `${minutes}`+':'+`${seconds}`;
 }
 
+$('.player__start').click(e => {
+  e.preventDefault();
+  
+  if (playerContainer.hasClass('paused')) {
+    playerContainer.removeClass('paused');
+    player.pause();
+  } else {
+    playerContainer.addClass('paused');
+    player.play();
+  }
+});
 
-player.getCurrentTime().then((seconds) => { 
+player.getDuration().then((duration) => {
+  $('.player__duration-estimate').text(formatTime(duration));
+  // `duration` indicates the duration of the video in seconds
+});
+
+player.on('play', () => {
   let interval;
 
   if (typeof interval !== 'undefined') {
@@ -24,33 +97,19 @@ player.getCurrentTime().then((seconds) => {
   }
 
   interval = setInterval(() => {
-    let completedSec = seconds;
-    $('.player__duration-completed').text(formatTime(completedSec));
-    // console.log(completedSec);
-  }, 1000);
-});
+    const currentTime = player.getCurrentTime().then(function(seconds) {
+      $('.player__duration-completed').text(formatTime(seconds));
+      // console.log(formatTime(seconds));
+    });
 
-
-
-const durationSec = player.getDuration().then((dur) => {
+    // $('.player__playback-button').css({
+    //   left: `${5}%`
+    // });
+  },1000)
   
-  $('.player__duration-estimate').text(formatTime(dur));
+  console.log('Played the video');
 });
 
-let eventsInit = () => {
-
-  $('.player__start').click(e => {
-    e.preventDefault();
-
-    if (playerContainer.hasClass('paused')) {
-      playerContainer.removeClass('paused');
-      player.pause();
-    } else {
-      playerContainer.addClass('paused');
-      player.play();
-    }
-    
-  });
-};
-
-eventsInit();
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
